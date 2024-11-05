13f285e9056bd909f05432f3bf180503bc1dac78
Samp83 <sami.paris@supdevinci-edu.fr>
2024-11-05 11:55:14 +0100

    aa

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -1,8 +1,8 @@
 //import { useState } from 'react'
 
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
 import CounterButton from './components/counterbutton';
 import TableToto from './components/PlayerTable';
@@ -22,15 +22,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+
       <div className="card">
       <TableToto players={players} />
       <CounterButton />
