ed0367874aa2cfd4f07c01bddf880307567170f5
Samp83 <sami.paris@supdevinci-edu.fr>
2024-11-05 13:22:28 +0100

    a

--- Frontend/vite-project/src/App.jsx
+++ Frontend/vite-project/src/App.jsx
@@ -4,36 +4,24 @@
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import './App.css'
-import CounterButton from './components/counterbutton';
-import TableToto from './components/PlayerTable';
-import ToDoList from './components/ToDoList';
+// import CounterButton from './components/counterbutton';
+// import TableToto from './components/PlayerTable';
+// import ToDoList from './components/ToDoList';
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import Toto from './pages/toto'
 
-const players = [
-  {
-      id: 1,
-      score: 0,
-      name: "toto",
-  },
-];
 
 function App() {
-  //const [count, setCount] = useState(0)
-  
 
   return (
     <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/toto" element={<Toto />} />
+      </Routes>
 
-      <div className="card">
-      <TableToto players={players} />
-      <CounterButton />
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <ToDoList />
+      
     </>
   )
 }
---
+++ Frontend/vite-project/src/pages/toto.jsx
@@ -0,0 +1,5 @@
+export default function Toto() {
+    return (
+        <h1>Toto</h1>
+    );
+  }
